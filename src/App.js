94a624643d1686370cdfeb521855b53d2153e9e4
Chanwit Saisin <chanwit.sa@ku.th>
2021-04-20 18:46:10 +0700

    Update slideshow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Switch, Route} from "react-router-dom";
 import Home from './models/home'
-import SelectTable   from './models/selectTable'
 import Menu from './models/menu'
 import Order from './models/order'
 import Receipt from './models/receipt'
@@ -11,7 +10,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
-          <Route exact path="/selectTable" component={SelectTable} />
           <Route exaxt path="/menu" component={Menu}/>
           <Route exaxt path="/order" component={Order}/>
           <Route exact path="/receipt" component={Receipt}/>
--- src/models/home.js
+++ src/models/home.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import '../css/style.css'
-import TheHeader from '../components/TheHeader'
+import Header from '../components/header'
+import Footer from '../components/footer'
 import '../css/homePage.css'
-import news1 from '../image/news1.jpg'
-import news2 from '../image/news2.jpg'
-import news3 from '../image/news3.jpg'
+import { db } from "./../firebase"
+import { useState,useEffect } from 'react'
+import $ from "jquery";
 
 
 const Home =()=> {
     var slideIndex = 1;
-    function startSlide(){
-        window.onload = function afterPageLoad(){
-            showSlides(slideIndex)
-        }
-    }
     
     function plusSlides(n) {
         showSlides(slideIndex += n);
@@ -24,42 +20,64 @@ const Home =()=> {
     }
 
     function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("slides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
+        console.log(document.readyState)
+        if( document.readyState == 'complete' ) {
+            var i;
+            var slides = document.getElementsByClassName("slides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+        } else {
+            document.addEventListener('DOMContentLoaded', function () {
+                showSlides(slideIndex)
+            });
         }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex-1].style.display = "block";
-        dots[slideIndex-1].className += " active";
     }
 
+    const [newList, setNewList] = useState({
+        new: []
+    })
+
+    const downloadNew = async() => {
+        db.ref(`/new`).on('value', async snapshot => {
+            const data = await snapshot.val()
+            if (data) {
+                setNewList({
+                    new: Object.keys(data),
+                    data: data
+                })
+            }
+        })
+    }
+    useEffect(() => {
+        downloadNew()
+    }, db.ref(`/new`))
+
     return (
-        <div class='bgOrder' onLoad={startSlide()}>
-            <TheHeader/>
+        <div class='bgOrder' onLoad={()=>showSlides(slideIndex)}>
+            <Header/>
             <div class="center-box-container" >
                 <div class="slideshow-container">
-                    <div class="slides fade" >
-                        <div class="numbertext">1 / 3</div>
-                        <img src={news1} class='news-img'/>
-                    </div>
-                    <div class="slides fade">
-                        <div class="numbertext">2 / 3</div>
-                        <img src={news2} class='news-img' />
-                    </div>
-
-                    <div class="slides fade">
-                        <div class="numbertext">3 / 3</div>
-                        <img src={news3} class='news-img'/>
-                    </div>
+                    {newList.new.map((key,index) => {
+                        return(
+                            <div class="slides fade" >
+                                <img src={newList.data[key].img} class='news-img'/>
+                            </div>
+                        )
+                    }
+                    )}
 
                     <a class="prev" onClick={() => plusSlides(-1)}>&#10094;</a>
                     <a class="next" onClick={() => plusSlides(1)}>&#10095;</a>
+                    
                 </div>
                 <br/>
 
@@ -68,10 +86,10 @@ const Home =()=> {
                     <span class="dot" onClick={() => currentSlide(2)}></span>
                     <span class="dot" onClick={() => currentSlide(3)}></span>
                 </div>
+
+                <button type="submit" onClick={() =>window.location.href = '/menu'}>Get started</button>
             </div>
-            <div class='bottom-box'>
-                
-            </div>
+            <Footer/>
         </div>
     );
 }
